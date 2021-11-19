dfde5716ef495c938e11eef7ba07dcdd7e2ea622
Ryan Burns <ryankburns@gmail.com>
2021-11-18 20:58:37 -0800

    added get current user action

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -4,3 +4,35 @@ import {
     AUTH_USER,
     COMPLETE_AUTH,
 } from './actionTypes'
+
+const URL = 'http://localhost:3001'
+
+export const getCurrentUser = () => {
+    return dispatch => {
+        fetch(`${URL}/get_current_user`, {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then(resp => resp.json())
+        .then(response => {
+            if (response.error) {
+                console.log(response.error)
+            } else {
+                dispatch({
+                    type: SET_CURRENT_USER
+                    user: response
+                })
+            }
+        })
+        .catch(console.log)
+    }
+};
+
+export const signup = (credentials) => {
+    return (dispatch) => {
+        fetch(`${URL}/signup`)
+    }
+}
