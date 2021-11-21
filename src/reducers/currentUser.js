1827f867b5636788133f7ad8f1fce20458690ac2
Ryan Burns <ryankburns@gmail.com>
2021-11-20 19:52:48 -0800

    added currentUser reducer

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -4,3 +4,12 @@ import {
     BEGIN_AUTH,
     COMPLETE_AUTH
 } from "../actionTypes"
+
+export function currentUser(state = { currentUser: {}, authComplete: false ), action}) {
+    switch(action.type) {
+        case SET_CURRENT_USER:
+            return { ...state, currentUser: action.user, }
+        case CLEAR_CURRENT_USER:
+            return null
+    }
+}  
