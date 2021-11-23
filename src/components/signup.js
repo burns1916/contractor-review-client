e5260d393bf367bc4b01e83da3eaec8e646143ac
Ryan Burns <ryankburns@gmail.com>
2021-11-22 19:39:14 -0800

    finished signup component

--- src/components/signup.js
+++ src/components/signup.js
@@ -15,17 +15,32 @@ class SignUp extends Component {
     }
 
     handleChange = ({ target }) => {
-
+        this.setState({[target.name]: target.value})
     }
 
     handleOnSubmit = (e) => {
         e.preventDefault();
+        this.props.signup(this.state)
+        this.setState({
+            username: "",
+            password: "",
+        })
 
     }
 
     render() {
         return(
-
+            <>
+            { !this.props.loggedIn ?
+            <form onSubmit={this.handleSubmit}>
+                <label>Username:</label>
+                <input type="text" placeholder="username" value={this.state.username} onChange={this.handleChange} />
+                <label>Password:</label>
+                <input type="password" placeholder="password" value={this.state.password} onChange={this.handleChange} />
+                <input type="submit" value="Sign Up" />
+            </form>
+            : <Redirect to='/' />}
+            </>
         )
     }
 
